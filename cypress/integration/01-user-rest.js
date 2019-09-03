dfbf7ce0c14aba7191fd69e376085ff82118f766
ideabile <info@ideabile.com>
2019-09-04 01:13:43 +0200

    🧪  Add RESTful test for POST /user

--- cypress/integration/01-user-rest.js
+++ cypress/integration/01-user-rest.js
@@ -6,4 +6,30 @@ describe('User RESTful', () => {
 
   });
 
+  describe('Create a User', () => {
+
+    it('Should return StatusCode: 200, body: { id: 0  } to POST /user', () => {
+
+      const request = cy.request({
+        method: 'POST',
+        url: '/user',
+        followRedirect: false,
+        headers: {
+          'accept': 'application/json'
+        }
+      });
+
+      request.then(response => {
+
+        expect(response.status).to.eq(200);
+        return response;
+
+      });
+
+      request.its('body').should('deep.eq', { id: 0 });
+
+    });
+
+  });
+
 });
