a39724bb31c9466ab1bda28fc52a8393a110d254
gajduk-mansystems <andrej.gajduk@mansystems.de>
2019-09-06 08:45:49 +0200

    Added users endpoint

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ let app = express();
 const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/users', (req, res) => res.send([]))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
